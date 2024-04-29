62f07bb8eb514fb07a046ecb8a5a6a12d6674c0b
o-vilna <162020673+o-vilna@users.noreply.github.com>
2024-04-29 13:07:14 +0200

    create an array with three objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-alert('Hello world');
-let favoriteFood='Pizza';
-document.write(favoriteFood);
+let pokemonList = [];
+//Add Pokémon objects to the array
+pokemonList.push(
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']    
+    },
+    {
+        name:'Charmander',
+        height:6,
+        types:['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 5,
+        types: ['water']
+    }
+);
+console.log(pokemonList);
