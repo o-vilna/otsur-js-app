748b2f79f10210af58235bc522b73253c5089b99
o-vilna <162020673+o-vilna@users.noreply.github.com>
2024-05-01 18:41:14 +0200

    feat:add 'for loop' with 'if condition'
    deleted .push

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-let pokemonList = [];
-//Add Pokémon objects to the array
-pokemonList.push(
+let pokemonList = [
     {
         name: 'Bulbasaur',
         height: 7,
@@ -16,5 +14,13 @@ pokemonList.push(
         height: 5,
         types: ['water']
     }
-);
-console.log(pokemonList);
+];
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    let result = ' '; // Initialize result variable
+    if (pokemon.height > 6) {
+        result =  '- Wow, that’s big!'; // If height is greater than 6, set result to "Wow, that’s big!"
+      }
+      
+    document.write(pokemon.name + " (height: " + pokemon.height + result + ")<br>");
+    }
