b3de3fc45a8f7cc49a7bf2fb62a52f8494b05dd7
o-vilna <162020673+o-vilna@users.noreply.github.com>
2024-05-20 18:23:37 +0200

    replace"for" loop with a function forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
 let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass', 'poison']    
-    },
-    {
-        name:'Charmander',
-        height:6,
-        types:['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 5,
-        types: ['water']
-    }
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Charmander",
+    height: 6,
+    types: ["fire"],
+  },
+  {
+    name: "Squirtle",
+    height: 5,
+    types: ["water"],
+  },
 ];
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon = pokemonList[i];
-    let result = ' '; // Initialize result variable
-    if (pokemon.height > 6) {
-        result =  '- Wow, that’s big!'; // If height is greater than 6, set result to "Wow, that’s big!"
-      }
-      
-    document.write(pokemon.name + " (height: " + pokemon.height + result + ")<br>");
-    }
+pokemonList.forEach(function (pokemon) {
+  let result = "";
+  if (pokemon.height > 6) {
+    result = "- Wow, that’s big!"; // If height is greater than 6, set result to "Wow, that’s big!"
+  }
+  //console.log(pokemon.name + " (height: " + pokemon.height + result + ")");
+//});
+  document.write(
+    pokemon.name + " (height: " + pokemon.height + result + ")<br>"
+  );
+});
