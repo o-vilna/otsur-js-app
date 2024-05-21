927b49d113187c0395f58000b5adbfe6ba22f63b
o-vilna <162020673+o-vilna@users.noreply.github.com>
2024-05-21 11:20:00 +0200

    wrap the pokemonList array in an IIFE
    function add, function getAll()

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,39 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Charmander",
-    height: 6,
-    types: ["fire"],
-  },
-  {
-    name: "Squirtle",
-    height: 5,
-    types: ["water"],
-  },
-];
-pokemonList.forEach(function (pokemon) {
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charmander",
+      height: 6,
+      types: ["fire"],
+    },
+    {
+      name: "Squirtle",
+      height: 5,
+      types: ["water"],
+    },
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu" });
+pokemonRepository.getAll().forEach(function (pokemon) {
   let result = "";
   if (pokemon.height > 6) {
     result = "- Wow, that’s big!"; // If height is greater than 6, set result to "Wow, that’s big!"
   }
-  //console.log(pokemon.name + " (height: " + pokemon.height + result + ")");
-//});
   document.write(
     pokemon.name + " (height: " + pokemon.height + result + ")<br>"
   );
